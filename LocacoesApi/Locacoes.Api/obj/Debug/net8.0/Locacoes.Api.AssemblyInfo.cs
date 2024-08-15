750d896613c3accd268f3f8379f77e10084e3900
yurisanford <yurisanford@fortestecnologia.com.br>
2024-08-15 18:42:24 -0300

    Adicionando dependencias nos demais projetos

--- LocacoesApi/Locacoes.Api/obj/Debug/net8.0/Locacoes.Api.AssemblyInfo.cs
+++ LocacoesApi/Locacoes.Api/obj/Debug/net8.0/Locacoes.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Locacoes.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a0ceef0322e3fd9fe3207e2e1b68d4a280593d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa36e7a580daa0cf34a5deb5a51de7416c3d4fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Locacoes.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Locacoes.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
